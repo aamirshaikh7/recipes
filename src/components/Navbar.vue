626a9beaf9dd087823b91941ebf3c1d9a32fee36
Aamir shaikh <aamirshaikh3232@gmail.com>
2020-07-20 17:09:02 +0530

    Adding Navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="navbar"> 
+        <nav class="nav-extended">
+            <div class="nav-content">
+                <router-link to="">
+                    <span class="nav-title">Recipes</span>
+                </router-link>
+                <a href="" class="btn-floating halfway-fab black">
+                    <router-link to="">
+                        <i class="material-icons">add</i>
+                    </router-link>
+                </a>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+
+    data () {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+.navbar nav {
+    padding: 0 15px;
+}
+
+.navbar nav i {
+    line-height: 40px;
+}
+</style>
